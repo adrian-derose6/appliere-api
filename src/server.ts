f1b44408efe797012e5f0e816f680ee5066af14e
Adrian DeRose <adrian.derose6@gmail.com>
2022-04-18 22:37:06 -0500

    Initialize Project with TypeScript config

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from 'express';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+	console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
